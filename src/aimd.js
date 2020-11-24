d17e843c12e7fe551a5297a22d728abe5aed1b0f
Joshua Napoli <josh@cumulusds.com>
2020-11-23 22:30:48 -0500

    Add AIMD congestion algorithm

---
+++ src/aimd.js
@@ -0,0 +1,65 @@
+// @flow
+
+import util from "util";
+
+const setTimeoutPromise = util.promisify(setTimeout);
+
+type Params = {
+  // Additive factor (invocations per millisecond) for increasing function issue rate upon success
+  a: number,
+
+  // Multiplicative factor for decreasing function issue rate upon error
+  b: number,
+
+  // Initial function issue rate (invocations per millisecond)
+  w: number,
+
+  // Retry until deadline (milliseconds since epoch) would be exceeded
+  deadline: number
+};
+
+export class Timeout extends Error {
+  code = "Timeout";
+
+  constructor() {
+    super("Timeout");
+  }
+}
+
+// Return a thunk that invokes the given async function rate-limited using additive increase/multiplicative decrease congestion control. If the given function throws, then the thunk retries it until the deadline would be exceeded.
+export default function aimd(params: Params) {
+  const { a, b, deadline } = params;
+  let { w } = params;
+  let n = 0;
+  const start = Date.now();
+
+  return async function invoke<T>(fun: () => Promise<T>): Promise<T> {
+    let now = Date.now();
+    let elapsed = now - start;
+    let target = elapsed * w;
+    let shortfall = n - target;
+    let delay = shortfall / w;
+    while (now + delay < deadline) {
+      if (delay > 0) {
+        await setTimeoutPromise(delay);
+      }
+      n += 1;
+      try {
+        const result = await fun();
+        w += a;
+        return result;
+      } catch (err) {
+        w *= b;
+        now = Date.now();
+        elapsed = now - start;
+        target = elapsed * w;
+        shortfall = n - target;
+        delay = shortfall / w;
+        if (deadline <= now + delay) {
+          throw err;
+        }
+      }
+    }
+    throw new Timeout();
+  };
+}
